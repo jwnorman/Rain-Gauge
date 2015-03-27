ac527e93f7fdbd2e270043b2ccaf0ba91d340183
Jack Norman <jwnorman91@gmail.com>
2015-03-26 23:10:45 -0700

    missing values: added a few functions to deal with the different types of missing values

--- process.R
+++ process.R
@@ -3,7 +3,7 @@ library(data.table)
 trainExpected <- fread("train_2013.csv", select="Expected")
 
 # directory prefix
-directory <- "~/Documents/Kaggle/Rain/Rain-Gauge/Data"
+directory <- "~/Documents/Kaggle/Rain/Rain-Gauge/Data/"
 # load data
 tr <- fread(paste(directory, "train_2013.csv", sep=''), header = TRUE,
 			   colClasses = c("integer", "character", "character", "character", 
@@ -44,28 +44,55 @@ id_Lengths_te <- sapply(te[,2], function(x) { # 2 is arbitrary
 	length(unlist(strsplit(x, split=' ')))
 }, USE.NAMES = FALSE)
 
-# For the new dataset, the names will be the same except with an appended ".Unlisted"
-unlistedColNames_tr <- paste(names(tr)[2:19], ".Unlisted", sep='')
-unlistedColNames_te <- paste(names(te)[2:18], ".Unlisted", sep='')
-id_Unlisted_tr <- rep(tr$Id, id_Lengths_tr)
-id_Unlisted_te <- rep(te$Id, id_Lengths_te)
-Expected.Unlisted <- rep(tr$Expected, id_Lengths_tr)
+id_tr <- rep(tr$Id, id_Lengths_tr)
+id_te <- rep(te$Id, id_Lengths_te)
+Expected <- rep(tr$Expected, id_tr)
 
 # Create jumbo data frame
-tr.Unlisted <- data.frame(id_Unlisted_tr, matrix(0, nrow=length(id_Unlisted_tr), ncol=length(unlistedColNames_tr)), Expected.Unlisted)
-names(tr.Unlisted)[2:19] <- unlistedColNames_tr
+tr.Unlisted <- data.frame(id_tr, matrix(0, nrow=length(id_tr), ncol=length(unlistedColNames_tr)), Expected)
 for (i in 2:19) {
 	tr.Unlisted[,i] <- unlist(strsplit(tr[,i], split=' '))
 }
 
-te.Unlisted <- data.frame(id_Unlisted_te, matrix(0, nrow=length(id_Unlisted_te), ncol=length(unlistedColNames_te)))
-names(te.Unlisted)[2:18] <- unlistedColNames_te
+te.Unlisted <- data.frame(id_te, matrix(0, nrow=length(id_te), ncol=length(unlistedColNames_te)))
 for (i in 2:18) {
 	te.Unlisted[,i] <- unlist(strsplit(te[,i], split=' '))
 }
 
 save(tr.Unlisted, file=paste(directory, "trUnlisted.Rda", sep=''))
 save(te.Unlisted, file=paste(directory, "teUnlisted.Rda", sep=''))
+
+# # Deal with NA's
+# # From the Kaggle data page, there are different types of missing values
+# # -99000, -99901, -99903, nan, 999
+
+# create column with factors of different types of missing
+# for example, HybridScan will be used to create come HybridScan.missing
+# not na: type0
+# -99900: type1
+# -99901: type2
+# -99903: type3
+# nan	: type4
+# 999	: type5
+
+variablesWithMissing <- apply(tr.Unlisted, MARGIN=2, function(x) {
+	any(x %in% c("-99900.0", "-99901.0", "-99903.0", "nan", "999.0"))
+})
+
+produceMissingVariable <- function(variable) {
+	na.as.factor <- ifelse(as.numeric(variable) == -99900.0, "type1",
+					ifelse(as.numeric(variable) == -99901.0, "type2",
+					ifelse(as.numeric(variable) == -99903.0, "type3",
+					ifelse(variable == "nan",  "type4",
+					ifelse(as.numeric(variable) == 999.0,    "type5",
+					"type0")))))
+	return(as.factor(na.as.factor))
+}
+
+replaceWithActualNA <- function(variable) {
+	varWithNA <- ifelse(variable %in% c("-99900.0", "-99901.0", "-99903.0", "nan", "999.0"), NA, variable)
+	return(varWithNA)
+}
 
 # According to competition director, there was a typo in Kdp so that Kdp is always 0
 # To calculate Kdp, use the following formula
