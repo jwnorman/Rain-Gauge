46a9aff6e917ccebc28a7e755e097ed0f48517cd
Jack Norman <jwnorman91@gmail.com>
2015-03-27 12:39:55 -0700

    corrected the classes of the variables in *.Unlisted; tedious and not elegant but its a one-time thing

--- process.R
+++ process.R
@@ -67,6 +67,48 @@ names(te.Unlisted) <- names(te)
 save(tr.Unlisted, file=paste(directory, "trUnlisted.Rda", sep=''))
 save(te.Unlisted, file=paste(directory, "teUnlisted.Rda", sep=''))
 
+## Correct the classes of all the variables in tr.Unlisted and te.Unlisted
+## Probably should've dealt with this when first making *.Unlisted
+tr.Unlisted.backup <- tr.Unlisted
+te.Unlisted.backup <- te.Unlisted
+tr.Unlisted$TimeToEnd <- as.integer(tr.Unlisted$TimeToEnd)
+tr.Unlisted$DistanceToRadar <- as.integer(tr.Unlisted$DistanceToRadar)
+tr.Unlisted$Composite <- as.numeric(tr.Unlisted$Composite)
+tr.Unlisted$HybridScan <- as.numeric(tr.Unlisted$HybridScan)
+tr.Unlisted$HydrometeorType <- as.factor(tr.Unlisted$HydrometeorType)
+tr.Unlisted$Kdp <- as.numeric(tr.Unlisted$Kdp)
+tr.Unlisted$RR1 <- as.numeric(tr.Unlisted$RR1)
+tr.Unlisted$RR2 <- as.numeric(tr.Unlisted$RR2)
+tr.Unlisted$RR3 <- as.numeric(tr.Unlisted$RR3)
+tr.Unlisted$RadarQualityIndex <- as.numeric(tr.Unlisted$RadarQualityIndex)
+tr.Unlisted$Reflectivity <- as.numeric(tr.Unlisted$Reflectivity)
+tr.Unlisted$ReflectivityQC <- as.numeric(tr.Unlisted$ReflectivityQC)
+tr.Unlisted$RhoHV <- as.numeric(tr.Unlisted$RhoHV)
+tr.Unlisted$Velocity <- as.numeric(tr.Unlisted$Velocity)
+tr.Unlisted$Zdr <- as.numeric(tr.Unlisted$Zdr)
+tr.Unlisted$LogWaterVolume <- as.numeric(tr.Unlisted$LogWaterVolume)
+tr.Unlisted$MassWeightedMean <- as.numeric(tr.Unlisted$MassWeightedMean)
+tr.Unlisted$MassWeightedSD <- as.numeric(tr.Unlisted$MassWeightedSD)
+
+te.Unlisted$TimeToEnd <- as.integer(te.Unlisted$TimeToEnd)
+te.Unlisted$DistanceToRadar <- as.integer(te.Unlisted$DistanceToRadar)
+te.Unlisted$Composite <- as.numeric(te.Unlisted$Composite)
+te.Unlisted$HybridScan <- as.numeric(te.Unlisted$HybridScan)
+te.Unlisted$HydrometeorType <- as.factor(te.Unlisted$HydrometeorType)
+te.Unlisted$Kdp <- as.numeric(te.Unlisted$Kdp)
+te.Unlisted$RR1 <- as.numeric(te.Unlisted$RR1)
+te.Unlisted$RR2 <- as.numeric(te.Unlisted$RR2)
+te.Unlisted$RR3 <- as.numeric(te.Unlisted$RR3)
+te.Unlisted$RadarQualityIndex <- as.numeric(te.Unlisted$RadarQualityIndex)
+te.Unlisted$Reflectivity <- as.numeric(te.Unlisted$Reflectivity)
+te.Unlisted$ReflectivityQC <- as.numeric(te.Unlisted$ReflectivityQC)
+te.Unlisted$RhoHV <- as.numeric(te.Unlisted$RhoHV)
+te.Unlisted$Velocity <- as.numeric(te.Unlisted$Velocity)
+te.Unlisted$Zdr <- as.numeric(te.Unlisted$Zdr)
+te.Unlisted$LogWaterVolume <- as.numeric(te.Unlisted$LogWaterVolume)
+te.Unlisted$MassWeightedMean <- as.numeric(te.Unlisted$MassWeightedMean)
+te.Unlisted$MassWeightedSD <- as.numeric(te.Unlisted$MassWeightedSD)
+
 # According to competition director, there was a typo in Kdp so that Kdp is always 0
 # To calculate Kdp, use the following formula
 abs(Kdp) = exp(log(abs(RR3)/40.6)/0.866)*(abs(RR3)/RR3)
