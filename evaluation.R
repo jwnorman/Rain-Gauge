8475d6fc78244e530d675df3abfa84eb6967048d
Jack Norman <jwnorman91@gmail.com>
2015-04-01 16:15:32 -0700

    updated crps evaluation function; still slow though

--- evaluation.R
+++ evaluation.R
@@ -1,17 +1,4 @@
 # evaluation
-rownum <- 102039
-CRPS <- function(cdfmat, actualGaugeValues) {
-	mean(
-		sapply(nrow(cdfmat), 
-			function(rownum) { # for every row
-				(cdfmat[rownum,] - H( (0:69) - actualGaugeValues[rownum] ))^2
-			}
-		)
-	)
-}
-CRPS(cdfmat, actualGaugeValues)
-cdfmat <- matrix(1, nrow = 8937958, ncol = 70)
-actualGaugeValues <- tr.Unlisted$Expected
 
 H <- function(x) {
 	ifelse(x < 0, 0, 1)
@@ -23,14 +10,20 @@ isNonDecreasing <- function(cdf) {
 
 submissionNames <- c("Id", paste("Predicted", 0:69, sep=''))
 
+CRPS <- function(cdfmat, actual) {
+	temp <- data.frame(cdfmat=cdfmat, actual=actual)
+	if (!isNonDecreasing(cdfmat)) {
+		stop("cdf isn't nondecreasing")
+	}
+	mean(apply(temp, MARGIN=1, function(temprow) {
+		mean((as.integer(temprow[1:70]) - H((0:69) - as.integer(temprow[71])))^2)
+	})	)
+}
 
-
-
-
-
-
-
-
-
+cdfmat <- matrix(1, nrow = 8937958, ncol = 70)
+actual <- tr.Unlisted$Expected
+cdfmattest <- head(cdfmat, 1200)
+actualtest <- head(actual, 1200)
+CRPS(cdfmat, actual)
 
 
