25d4f227fbafda5b83fa1ae0d19d15b59354bea3
Jack Norman <jwnorman91@gmail.com>
2015-04-07 16:27:31 -0700

    made a few changes to the import process of the csv file

--- extract.cpp
+++ extract.cpp
@@ -4,12 +4,13 @@
 #include <string>
 #include <cstring>
 #include <cstdio>
+#include <sstream>
 
 using namespace std;
 
 	// int MROWS = 1126695;
-	int NROWS = 100; // for testing purposes
-	int NCOLS = 19;
+	int NROWS = 3; // for testing purposes
+	int NCOLS = 20;
 
 int main(int argc, const char * argv[]) {
 	if (argc == 1) {
@@ -17,25 +18,27 @@ int main(int argc, const char * argv[]) {
 		return(0);
 	} 
 
-	// work variables
 	ifstream datafile;
-	int lineCounter = 1;
-
-	// string variables
+	stringstream iss;
+	string linetemp;
 	string temp;
-
-	char * pch; 
-
-
-
-	// there needs to be a file given
-
+	string colNames[NCOLS]; 
 
 	datafile.open(argv[1]);
-	while (datafile.good() & (lineCounter <= NROWS)) {
-		getline(datafile, temp, ',');
-		cout << temp << endl;
-		lineCounter++;
+	if (datafile.good()) {
+		for (int nrow = 1; nrow <= NROWS; nrow++) {
+			getline(datafile, linetemp);
+			iss << linetemp;
+			for (int ncol = 1; ncol <= NCOLS; ncol++) {
+				getline(iss, temp, ',');
+				if (nrow == 1) colNames[ncol-1] = temp; // assign column names
+				cout << colNames[ncol-1] << endl;
+				cout << "nrow: " << nrow << endl;
+				cout << "ncol: " << ncol << endl;
+				cout << temp << endl << endl << endl;
+			}
+			getline(iss, temp); // extra to grab the number?
+		}
 	}
 	datafile.close();
 }
