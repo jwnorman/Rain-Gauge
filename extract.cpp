fe9b7c026367fd77378fb60bc2377e3ea82c82d9
Jack Norman <jwnorman91@gmail.com>
2015-04-10 13:19:21 -0700

    instead of storing the summary results in a huge matrix (which will not be able to be allocated because it would be too large), the summary results are printed to a file immediately

--- extract.cpp
+++ extract.cpp
@@ -22,18 +22,20 @@ int main(int argc, const char * argv[]) {
 		cout << "No argument given; the data file to be read needs to be given." << endl;
 		return(0);
 	} 
-	int NROWS = 10000;
+	int NROWS = 1126695;
 	int NCOLS = 20;
 	ifstream datafile;
+	ofstream rainSummary;
 	string linetemp;
 	string temp;
 	string *colNames = new string[NCOLS];
 	string funNames[] = {"mean", "range"};
 	// int *dataCollector = new int[NROWS][2*20]; // 2 corresponds to number of functions; 20 corresponds to number of columns. How do do this dynamicall? Vectors I guess...
-	double dataCollector[10000][2*20]; // hardcoded
+	// double dataCollector[5][2*20]; // too large to allocate when NROWS is large!
 	double statsTemp[2]; // hardcoded
 
 	datafile.open(argv[1]);
+	rainSummary.open ("rainSummary.csv");
 	if (datafile.good()) {
 		for (int nrow = 0; nrow < NROWS; nrow++) {
 			stringstream iss; // needed in for loop to be reset. what's the better way to reset?
@@ -44,26 +46,28 @@ int main(int argc, const char * argv[]) {
 				if (nrow == 0) { // grab header names
 					colNames[ncol] = temp;
 				} else {
-					// cout << colNames[ncol-1] << endl;
-					// cout << "nrow: " << nrow + 1<< endl;
-					// cout << "ncol: " << ncol + 1<< endl;
-					// cout << temp << endl;
 					calculate(temp, statsTemp);
-					// cout << "storage mean: " << statsTemp[0] << endl;
-					// cout << "storage range: " << statsTemp[1] << endl << endl;
-					dataCollector[nrow][ncol*2] = statsTemp[0];
-					dataCollector[nrow][ncol*2+1] = statsTemp[1];
+					// dataCollector[nrow][ncol*2] = statsTemp[0];
+					// dataCollector[nrow][ncol*2+1] = statsTemp[1];
+					if (ncol*2+1 != NCOLS*2-1) {
+						rainSummary << statsTemp[0] << ",";
+						rainSummary << statsTemp[1] << ",";
+					} else {
+						rainSummary << statsTemp[0] << ",";
+						rainSummary << statsTemp[1] << endl;
+					}
+					
 				} // else
 			}  // for
 		} // if
 	}
 	datafile.close();
+	rainSummary.close();
 	// printData(colNames, dataCollector, NROWS);
-	write2File(dataCollector, NROWS);
+	// write2File(dataCollector, NROWS);
 }
 
 void calculate(string numbers, double (&storage)[2]) { // hardcoded
-	//storage[0] = 0.0; storage[1] = 1.0; storage[2] = 2.0;
 	// break string to numbers
 	vector<string> tokens;
 	istringstream iss(numbers);
@@ -90,12 +94,10 @@ double calcMean(vector<string> numbers) {
 		}
 	}
 	mean = (n > 0) ? (sum / n) : (-99900.0);
-	//cout << "mean: " << mean << endl;
 	return(mean);
 }
 
 double calcRange(vector<string> numbers) {
-	//vector<double> numsWOmissing;
 	double range = 0.0;
 	double min =  99999;
 	double max = -99999;
@@ -103,7 +105,6 @@ double calcRange(vector<string> numbers) {
 		 it != numbers.end();
 		 it++) {
 		if (!(*it == "-99900.0" || *it == "-99901.0" || *it == "-99902" || *it == "nan" || *it == "999.0" || *it == "" || *it == " " || *it == "\n")) {
-			//numsWOmissing.push_back(stod(*it));
 			if ((stod(*it) < min)) {
 				min = stod(*it);
 			}
@@ -117,18 +118,10 @@ double calcRange(vector<string> numbers) {
 	} else {
 		range = -99900.0;
 	}
-	//cout << "range: " << range << endl;
 	return(range);
 }
 
 void printData(string headers[], double stats[][2*20], int nr) {
-	// for (int h = 0; h < 19; h++) { // hardcoded
-	// 	cout << headers[h] << ".mean,";
-	// 	cout << headers[h] << ".range,";
-	// }
-	// cout << headers[19] << ".mean,";
-	// cout << headers[19] << ".range";
-	// cout << "\n";
 	for (int i = 1; i < nr; i++) {
 		for (int j=0; j < 2*19-1; j++) { // hardcoded
 			cout << stats[i][j] << ",";
