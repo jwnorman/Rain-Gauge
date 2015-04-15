a6f3538725f9272f241c46cd0a6c704cb8d7ace2
Jack Norman <jwnorman91@gmail.com>
2015-04-15 14:58:52 -0700

    deal with NAs by mode from kernel density estimation

--- explore_v2.R
+++ explore_v2.R
@@ -8,6 +8,8 @@ directory <- "C://Kaggle - other//Rain//Rain-Gauge//Data//" # PC
 # load existing data
 load(file=paste(directory, "tr.Rda", sep=''))
 load(file=paste(directory, "te.Rda", sep=''))
+load(file=paste(directory, "train.Rda", sep=''))
+load(file=paste(directory, "test.Rda", sep=''))
 
 # # # How does explnatory variables interact with other?
 cortab <- cor(tr, use="complete.obs")
@@ -22,7 +24,7 @@ NApercent.te <- apply(tr, MARGIN=2, function(x) {
 })
 round(NApercent.tr, 4)
 round(NApercent.te, 4)
-round((NApercent.tr + NApercent.te)/2, 2)
+round((NApercent.tr + NApercent.te)/2, 4)
 
 # # # Numeric, low missing dataset
 train <- tr[,c("Id.mean", "RR1.mean", "RR1.range", "Reflectivity.mean", "Reflectivity.range", "RhoHV.mean", "RhoHV.range", "Expected.mean")]
@@ -36,3 +38,40 @@ fit <- glm(ebin ~ rr1m + rr1r + rm + rr + rhvm + rhvr, family = "binomial", data
 summary(fit)
 
 # # # Deal with NAs by surrogates or max densities
+# By max density
+combined <- as.data.frame(rbind(train[,2:(ncol(train)-1)], test[,2:(ncol(test))]))
+apply(train, MARGIN=2, function(x){ length(which((is.na(x)))) })
+apply(test, MARGIN=2, function(x){ length(which((is.na(x)))) })
+apply(combined, MARGIN=2, function(x){ length(which((is.na(x)))) })
+
+# find max densities of each variable
+maxDensities <- apply(combined, MARGIN=2, function(col) {
+	densTemp <- density(col, na.rm=TRUE)
+	densTemp$x[densTemp$y == max(densTemp$y)]
+})
+backuptrain <- train
+backuptest <- test
+
+# replace NAs, and split back in to train and test
+combined$rr1m <- ifelse(is.na(combined$rr1m), maxDensities["rr1m"], combined$rr1m)
+combined$rr1r <- ifelse(is.na(combined$rr1r), maxDensities["rr1r"], combined$rr1r)
+combined$rm <- ifelse(is.na(combined$rm), maxDensities["rm"], combined$rm)
+combined$rr <- ifelse(is.na(combined$rr), maxDensities["rr"], combined$rr)
+combined$rhvm <- ifelse(is.na(combined$rhvm), maxDensities["rhvm"], combined$rhvm)
+combined$rhvr <- ifelse(is.na(combined$rhvr), maxDensities["rhvr"], combined$rhvr)
+
+# Bring back in Id and Expected
+train <- combined[1:(nrow(train)), ]
+test <- combined[(nrow(train)+1):nrow(combined), ]
+train$id <- tr$Id.mean
+test$id <- te$Id.mean
+train$e <- tr$Expected.mean
+
+# Save
+save(train, file=paste(directory, "train.Rda", sep=''))
+save(test, file=paste(directory, "test.Rda", sep=''))
+
+
+
+
+
