4c5cd32879623a1af1504ac79e339fa531a5fbfa
Jack Norman <jwnorman91@gmail.com>
2015-04-22 13:12:45 -0700

    add code to bring in hydrometeor type mode data

--- process.R
+++ process.R
@@ -18,9 +18,9 @@ save(largetr, file=paste(directory, "largetr.Rda", sep=''))
 save(largete, file=paste(directory, "largete.Rda", sep=''))
 
 # import data after summarizing using extract.cpp
-trainFileName <- "meanDiffTrain.csv"
-testFileName <- "meanDiffTest.csv"
-headerExtension <- ".diffMean"
+trainFileName <- "hydroModeTrain.csv"
+testFileName <- "hydroModeTest.csv"
+headerExtension <- ".hydroMode"
 
 tr.header <- read.table(paste(directory, "train_2013.csv", sep=''), sep=',', nrow=1, header=FALSE, stringsAsFactors=FALSE)
 te.header <- read.table(paste(directory, "test_2014.csv", sep=''), sep=',', nrow=1, header=FALSE, stringsAsFactors=FALSE)
@@ -28,25 +28,27 @@ tr.header <- paste(rep(tr.header, each=1), headerExtension, sep='')
 te.header <- paste(rep(te.header, each=1), headerExtension, sep='')
 			
 extractedTrain <- fread(paste(directory, trainFileName, sep=''), 
-			sep = ',',
+			# sep = ',',
 			header = FALSE,
 			colClasses = "numeric",
 			nrows = 1126694)
 
 extractedTest <- fread(paste(directory, testFileName, sep=''), 
-			sep = ',',
+			# sep = ',',
 			header = FALSE,
 			colClasses = "numeric",
 			nrow = 630452)
 			
 extractedTrain <- as.data.frame(extractedTrain)
 extractedTest <- as.data.frame(extractedTest)
-names(extractedTrain) <- tr.header
-names(extractedTest) <- te.header
+# names(extractedTrain) <- tr.header
+# names(extractedTest) <- te.header
+names(extractedTrain) <- "hydroMode" #hydromode
+names(extractedTest) <- "hydroMode" #hydromode
 
 # Assign newly extracted data to savable variable
-meanDiffTrain <- extractedTrain
-meanDiffTest <- extractedTest
+hydroModeTrain <- extractedTrain
+hydroModeTest <- extractedTest
 
 # Get rid of useless variables and rename poorly named variables
 
@@ -70,8 +72,8 @@ meanRangeTrainFiltered <- tr[ , variablesToKeepTrain]
 meanRangeTestFiltered <-  te[ , variablesToKeepTest]
 
 # combine all variables into one df
-largetr <- as.data.frame(cbind(meanRangeTrainFiltered, meanDiffTrainFiltered))
-largete <- as.data.frame(cbind(meanRangeTestFiltered, meanDiffTestFiltered))
+largetr <- as.data.frame(cbind(meanRangeTrainFiltered, meanDiffTrainFiltered, hydroModeTrain))
+largete <- as.data.frame(cbind(meanRangeTestFiltered, meanDiffTestFiltered, hydroModeTest))
    				  
 # According to competition director, there was a typo in Kdp so that Kdp is always 0
 # To calculate Kdp, use the following formula
