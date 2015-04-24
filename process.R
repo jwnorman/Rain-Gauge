89e31e4314df3ca2054f5b2be677f8ce09d21724
Jack Norman <jwnorman91@gmail.com>
2015-04-24 14:16:59 -0700

    mini change: added braces to allow myself to minimize sections of code in the R editor

--- process.R
+++ process.R
@@ -5,7 +5,8 @@ library(data.table)
 directory <- "~/Documents/Kaggle/Rain/Rain-Gauge/Data/" # Mac
 directory <- "C://Kaggle - other//Rain//Rain-Gauge//Data//" # PC
 
-# load existing data
+# load / save existing data
+{
 load(file=paste(directory, "tr.Rda", sep=''))
 load(file=paste(directory, "te.Rda", sep=''))
 load(file=paste(directory, "largetr.Rda", sep=''))
@@ -22,8 +23,10 @@ save(largetr, file=paste(directory, "largetr.Rda", sep=''))
 save(largete, file=paste(directory, "largete.Rda", sep=''))
 save(train, file=paste(directory, "train.Rda", sep=''))
 save(test, file=paste(directory, "test.Rda", sep=''))
+}
 
 # import data after summarizing using extract.cpp
+{
 trainFileName <- "hydroModeTrain.csv"
 testFileName <- "hydroModeTest.csv"
 headerExtension <- ".hydroMode"
@@ -47,17 +50,16 @@ extractedTest <- fread(paste(directory, testFileName, sep=''),
 			
 extractedTrain <- as.data.frame(extractedTrain)
 extractedTest <- as.data.frame(extractedTest)
-# names(extractedTrain) <- tr.header
-# names(extractedTest) <- te.header
-names(extractedTrain) <- "hydroMode" #hydromode
-names(extractedTest) <- "hydroMode" #hydromode
+names(extractedTrain) <- "hydroMode"
+names(extractedTest) <- "hydroMode"
 
 # Assign newly extracted data to savable variable
 hydroModeTrain <- extractedTrain
 hydroModeTest <- extractedTest
+}
 
 # Get rid of useless variables and rename poorly named variables
-
+{
 # grab ids and target
 idTrain <- tr$Id.mean
 idTest <- te$Id.mean
@@ -80,13 +82,17 @@ meanRangeTestFiltered <-  te[ , variablesToKeepTest]
 # combine all variables into one df
 largetr <- as.data.frame(cbind(meanRangeTrainFiltered, meanDiffTrainFiltered, hydroModeTrain))
 largete <- as.data.frame(cbind(meanRangeTestFiltered, meanDiffTestFiltered, hydroModeTest))
+}
 
-# Numeric, low missing dataset
+# Keep variables with very few missing obserations
+{
 variablesToKeep <- c("RR1.mean", "RR1.range", "Reflectivity.mean", "Reflectivity.range", "RhoHV.mean", "RhoHV.range", "Zdr.mean", "Zdr.range", "RR1.diffMean", "Reflectivity.diffMean", "RhoHV.diffMean", "Zdr.diffMean", "hydroMode")
 train <- largetr[, variablesToKeep]
 test <- largete[, variablesToKeep]
+}
 
 # Deal with NAs by surrogates or max densities
+{
 # By max density
 combined <- as.data.frame(rbind(train, test))
 apply(train, MARGIN=2, function(x){ length(which((is.na(x)))) })
@@ -110,7 +116,10 @@ combined$hydroMode <- as.factor(combined$hydroMode)
 # Bring back in Id and Expected
 train <- as.data.frame(combined[1:(nrow(train)), ])
 test <- as.data.frame(combined[(nrow(train)+1):nrow(combined), ])
-   				  
+}
+
+# Deal with Kdp
+{
 # According to competition director, there was a typo in Kdp so that Kdp is always 0
 # To calculate Kdp, use the following formula
 # Run these after you take care of NAs
@@ -118,3 +127,4 @@ tr$Kdp.mean <- exp(log(abs(tr$RR3.mean)/40.6)/0.866)*sign(tr$RR3.mean)
 te$Kdp.mean <- exp(log(abs(te$RR3.mean)/40.6)/0.866)*sign(te$RR3.mean)
 tr$Kdp.range <- exp(log(abs(tr$RR3.range)/40.6)/0.866)*sign(tr$RR3.range)
 te$Kdp.range <- exp(log(abs(te$RR3.range)/40.6)/0.866)*sign(te$RR3.range)
+}
