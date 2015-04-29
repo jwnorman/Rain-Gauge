1431ec10aca17ccdc30c694ac84e7dda828ae3da
Jack Norman <jwnorman91@gmail.com>
2015-04-29 12:03:05 -0700

    rewrote CRPS function to calculate correctly; added opp to use parallel; wrote a script to build a model with the training dataset and calculate CRPS using crossvalidation

--- evaluation.R
+++ evaluation.R
@@ -1,29 +1,61 @@
 # evaluation
 
+library(parallel)
+
+model <- logit70 # model returns in submittable format
+keepVariables <- c("RR1.mean", "RR1.range", "Reflectivity.mean", "Reflectivity.range", "RhoHV.mean", "RhoHV.range")
+data <- train[ , keepVariables]
+data$Expected <- tr$Expected.mean
+data$Id <- tr$Id.mean
+data <- data[sample(x = nrow(data), size = 10000, replace = FALSE), ]
+cvk <- 10
+nobs <- floor(nrow(data) / cvk)
+mmmax <- 69
+dfsplitup <- lapply(1:cvk, function(block) {
+	data[sample(x = 1:nrow(data), size = nobs, replace = FALSE), ]
+})
+begin <- Sys.time()
+crpslist <- sapply(1:cvk, function(testnum) {
+	trnums <- (1:10)[-testnum]
+	trtemp <- do.call(rbind, dfsplitup[trnums])
+	tetemp <- dfsplitup[[testnum]]
+	predtemp <- model(trtemp, tetemp, mmmax)
+	CRPS(predtemp[ , -1], tetemp$Expected, cls=TRUE)
+})
+end <- Sys.time()
+tot <- end - begin
+
 H <- function(x) {
 	ifelse(x < 0, 0, 1)
 }
 
-isNonDecreasing <- function(cdf) {
-	return(all(diff(cdf) >= 0))
+isNonDecreasing <- function(cdfs) {
+	cdfs <- as.matrix(cdfs)
+	all(apply(cdfs, MARGIN = 1, function(row) {
+		all(diff(row) >= 0)
+	}))
 }
 
-submissionNames <- c("Id", paste("Predicted", 0:69, sep=''))
-
-CRPS <- function(cdfmat, actual) {
-	temp <- data.frame(cdfmat=cdfmat, actual=actual)
+CRPS <- function(cdfmat, actual, cls = FALSE) {
 	if (!isNonDecreasing(cdfmat)) {
 		stop("cdf isn't nondecreasing")
 	}
-	mean(apply(temp, MARGIN=1, function(temprow) {
-		mean((as.integer(temprow[1:70]) - H((0:69) - as.integer(temprow[71])))^2)
-	})	)
+	temp <- data.frame(cdfmat=cdfmat, actual=actual)
+	mms <- matrix(rep(0:69, 1000), nrow = 1000, byrow = TRUE)
+	if (!cls) {
+		# sum(sapply(1:nrow(temp), function(row) {
+			# sum(sapply(1:70, function(col) {
+				# (temp[row, col] - H(col - mms[col]))^2
+			# }))
+		# })) / (70*nrow(temp))
+		mean(sapply(1:nrow(temp), function(row) {
+			mean((temp[row, 0:70] - H(mms[row, ] - actual[row]))^2)
+		}))
+	} else {
+		numNodes <- detectCores() - 1
+		cls <- makeCluster(numNodes, type="FORK")
+		mean(parSapply(cls, 1:nrow(temp), function(row) {
+			mean((temp[row, 0:70] - H(mms[row, ] - actual[row]))^2)
+		}))
+	}
 }
-
-cdfmat <- matrix(1, nrow = 8937958, ncol = 70)
-actual <- tr.Unlisted$Expected
-cdfmattest <- head(cdfmat, 1200)
-actualtest <- head(actual, 1200)
-CRPS(cdfmat, actual)
-
-
