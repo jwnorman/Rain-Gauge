fa4762023c19c67b4049088b6e696ed54d9b92db
Jack Norman <jwnorman91@gmail.com>
2015-04-29 12:35:41 -0700

    turned scriptiness in to reusable function called getCRPS; cleaned up some comments and other code

--- evaluation.R
+++ evaluation.R
@@ -2,29 +2,6 @@
 
 library(parallel)
 
-model <- logit70 # model returns in submittable format
-keepVariables <- c("RR1.mean", "RR1.range", "Reflectivity.mean", "Reflectivity.range", "RhoHV.mean", "RhoHV.range")
-data <- train[ , keepVariables]
-data$Expected <- tr$Expected.mean
-data$Id <- tr$Id.mean
-data <- data[sample(x = nrow(data), size = 10000, replace = FALSE), ]
-cvk <- 10
-nobs <- floor(nrow(data) / cvk)
-mmmax <- 69
-dfsplitup <- lapply(1:cvk, function(block) {
-	data[sample(x = 1:nrow(data), size = nobs, replace = FALSE), ]
-})
-begin <- Sys.time()
-crpslist <- sapply(1:cvk, function(testnum) {
-	trnums <- (1:10)[-testnum]
-	trtemp <- do.call(rbind, dfsplitup[trnums])
-	tetemp <- dfsplitup[[testnum]]
-	predtemp <- model(trtemp, tetemp, mmmax)
-	CRPS(predtemp[ , -1], tetemp$Expected, cls=TRUE)
-})
-end <- Sys.time()
-tot <- end - begin
-
 H <- function(x) {
 	ifelse(x < 0, 0, 1)
 }
@@ -41,21 +18,64 @@ CRPS <- function(cdfmat, actual, cls = FALSE) {
 		stop("cdf isn't nondecreasing")
 	}
 	temp <- data.frame(cdfmat=cdfmat, actual=actual)
-	mms <- matrix(rep(0:69, 1000), nrow = 1000, byrow = TRUE)
+	mms <- matrix(rep(0:69, nrow(temp)), nrow = nrow(temp), byrow = TRUE)
 	if (!cls) {
-		# sum(sapply(1:nrow(temp), function(row) {
-			# sum(sapply(1:70, function(col) {
-				# (temp[row, col] - H(col - mms[col]))^2
-			# }))
-		# })) / (70*nrow(temp))
 		mean(sapply(1:nrow(temp), function(row) {
 			mean((temp[row, 0:70] - H(mms[row, ] - actual[row]))^2)
 		}))
 	} else {
 		numNodes <- detectCores() - 1
 		cls <- makeCluster(numNodes, type="FORK")
-		mean(parSapply(cls, 1:nrow(temp), function(row) {
+		crps <- mean(parSapply(cls, 1:nrow(temp), function(row) {
 			mean((temp[row, 0:70] - H(mms[row, ] - actual[row]))^2)
 		}))
+		stopCluster(cls)
+		return(crps)
 	}
 }
+
+logit70 <- function(trtemp, tetemp, mmmax=69) {
+	probsByMM <- sapply(0:mmmax, function(mm) {
+		tempExpected <- ifelse(trtemp$Expected >= (mm - .5) & trtemp$Expected <= (mm + .5), 1, 0)
+		tempFit <- glm(tempExpected ~ . - Id, family = "binomial", data = trtemp)
+		predict(tempFit, tetemp, type="response")
+	})
+	probsByMM <- cbind(probsByMM, matrix(0, nrow = nrow(tetemp), ncol = 70 - ncol(probsByMM)))
+	allCumsums <- t(apply(probsByMM, MARGIN=1, cumsum))
+	cdfs <- as.data.frame(t(apply(allCumsums, MARGIN=1, function(obs) obs/obs[length(obs)])))
+	cdfs$Id <- as.integer(tetemp$Id)
+	cdfs <- cdfs[,c(ncol(cdfs), 1:(ncol(cdfs)-1))]
+	names(cdfs) <- c("Id", paste("Predicted", 0:69, sep=''))
+	return(cdfs)
+}
+
+getCRPS <- function(model = logit70, data = data, dataSize = nrow(size), cvk = 10, mmmax = 69, cls = FALSE) {
+	data <- data[sample(x = nrow(data), size = dataSize, replace = FALSE), ]
+	nobs <- floor(nrow(data) / cvk)
+	dfsplitup <- lapply(1:cvk, function(block) {
+		data[sample(x = 1:nrow(data), size = nobs, replace = FALSE), ]
+	})
+	begin <- Sys.time()
+	crpslist <- sapply(1:cvk, function(testnum) {
+		trnums <- (1:10)[-testnum]
+		trtemp <- do.call(rbind, dfsplitup[trnums])
+		tetemp <- dfsplitup[[testnum]]
+		predtemp <- model(trtemp, tetemp, mmmax)
+		CRPS(predtemp[ , -1], tetemp$Expected, cls = cls)
+	})
+	end <- Sys.time()
+	tot <- end - begin
+	list(time = tot, crps = crpslist, crpsavg = mean(crpslist))
+}
+
+keepVariables <- c("RR1.mean", "RR1.range", "Reflectivity.mean", "Reflectivity.range", "RhoHV.mean", "RhoHV.range")
+data <- train[ , keepVariables]
+data$Expected <- tr$Expected.mean
+data$Id <- tr$Id.mean
+getCRPS(data = data, dataSize = 100, mmmax = 5, cls = TRUE)
+
+# 5.223747 minutes, serial, 6 variables, cvk = 10, mmmax = 69, size of all data used = 10000
+# 2.456471 minutes, parall, 6 variables, cvk = 10, mmmax = 69, size of all data used = 10000
+# 0.597540 minutes, parall, 6 variables, cvk = 10, mmmax = 10, size of all data used = 10000
+# 0.061651 minutes, parall, 6 variables, cvk = 10, mmmax = 5 , size of all data used = 100
+
